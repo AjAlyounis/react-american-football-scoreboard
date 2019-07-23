6decb845e44afce8b0b3c3504b08c978f750ddb6
Aj <Aj.alyounis@gmail.com>
2019-07-22 19:03:21 -0700

    added new content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+  const [homeScore, SetHomeScore] = useState(32);
+  // const [AwaytScore, SetHomeScore] = useState();
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
+          {/* Home Team */}
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">32</div>
+            <div className="home__score" onClick={() => SetHomeScore + 1} />
+            {homeScore}
           </div>
           <div className="timer">00:03</div>
+
+          {/* Away Team  */}
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">32</div>
@@ -32,8 +35,6 @@ function App() {
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
           <button className="awayButtons__touchdown">Away Touchdown</button>
           <button className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
